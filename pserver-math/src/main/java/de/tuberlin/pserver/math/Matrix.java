423b55ebd081d456cb9b0dd13cb3ada22f91b8e6
f-sander <fsander@mailbox.tu-berlin.de>
2015-07-08 16:16:21 +0200

    inplace/explace variants for all operations + javadoc

--- pserver-math/src/main/java/de/tuberlin/pserver/math/Matrix.java
+++ pserver-math/src/main/java/de/tuberlin/pserver/math/Matrix.java
@@ -152,23 +152,173 @@ public interface Matrix extends MObject {
 
     public abstract Matrix axpy(final double alpha, final Matrix B);        // A = alpha * B + A
 
-    public abstract Matrix add(final Matrix B);                             // A = B + A
+    /**
+     * Called on Matrix A. Computes Matrix-Matrix-Addition A += B and returns A.
+     * @param B Matrix to add on A
+     * @return A after computing A += B
+     */
+    public abstract Matrix add(final Matrix B);
 
-    public abstract Matrix sub(final Matrix B);                                             // A = B - A
+    /**
+     * Called on Matrix A. Computes Matrix-Matrix-Addition C = A + B and returns C.
+     * @param B Matrix to add on A
+     * @param C Matrix to store the result in
+     * @return C after computing C = A + B
+     */
+    public abstract Matrix add(final Matrix B, final Matrix C);
 
-    public abstract Matrix mul(final Matrix B);                                             // A = B * A
+    /**
+     * Called on Matrix A. Computes Matrix-Matrix-Subtraction A -= B and returns A.
+     * @param B Matrix to subtract from A
+     * @return A after computing A -= B
+     */
+    public abstract Matrix sub(final Matrix B);
 
-    public abstract Vector mul(final Vector B);                                             //
+    /**
+     * Called on Matrix A. Computes Matrix-Matrix-Subtraction C = A - B and returns C.
+     * @param B Matrix to subtract from A
+     * @param C Matrix to store the result in
+     * @return C after computing C = A - B
+     */
+    public abstract Matrix sub(final Matrix B, final Matrix C);
 
-    public abstract void mul(final Vector x, final Vector y);                               // y = A * x
+    /**
+     * Called on Matrix A. Computes Matrix-Matrix-Multiplication A *= B and returns A.
+     * @param B Matrix to multiply with A
+     * @return A after computing A *= B, or a new Matrix C after computing C = A * B, if the shape of A is not compatible to the resulting shape of A * B
+     * @throws IllegalStateException If the shape of A is incompatible to the resulting shape of A * B
+     */
+    public abstract Matrix mul(final Matrix B);
 
-    public abstract Matrix scale(final double alpha);                                       // A = alpha * A
+    /**
+     * Called on Matrix A. Computes Matrix-Matrix-Multiplication C = A * B and returns C.
+     * @param B Matrix to multiply with A
+     * @param C Matrix to store the result in
+     * @return C after computing C = A * B
+     */
+    public abstract Matrix mul(final Matrix B, final Matrix C);
 
-    public abstract Matrix transpose();                                                     // A = A^T
+    /**
+     * Called on Matrix A. Computes Matrix-Vector-Multiplication c = A * b and returns c.
+     * @param b Vector to multiply with A
+     * @param c c after computing c = A * b
+     */
+    public abstract void mul(final Vector b, final Vector c);
 
-    public abstract void transpose(final Matrix B);                                         // B = A^T
+    /**
+     * Called on Matrix A. Computes Matrix-Scalar-Multiplication A *= a
+     * @param a Scalar to multiply with A
+     * @return A after computing A *= a
+     */
+    public abstract Matrix scale(final double a);
 
-    public abstract boolean invert();                                                       // A = A^-1
+    /**
+     * Called on Matrix A. Computes Matrix-Scalar-Multiplication B = A * a
+     * @param a Scalar to multiply with A
+     * @param B Matrix to store the result in
+     * @return B after computing B = A * a
+     */
+    public abstract Matrix scale(final double a, final Matrix B);
+
+    /**
+     * Called on Matrix A. Computes transpose of A: A = A<sup>T</sup><br>
+     * <strong>Only succeeds for quadratic matrices!</strong>
+     * @return A after computing A<sup>T</sup>
+     * @throws IllegalStateException If the shape of A is incompatible to the resulting shape of A<sup>T</sup>
+     */
+    public abstract Matrix transpose();
+
+    /**
+     * Called on Matrix A. Computes transpose of A: B = A<sup>T</sup>
+     * @param B to store the result in
+     * @return B after computing B = A<sup>T</sup>
+     */
+    public abstract Matrix transpose(final Matrix B);
+
+    /**
+     * Called on Matrix A. Computes inverse of A: A = A<sup>-1</sup>
+     * @return A after computing A = A<sup>-1</sup>
+     * @throws IllegalStateException If A is singular an its inverse can not be computed
+     * @throws IllegalStateException If the shape of A is incompatible to the resulting shape of A<sup>-1</sup>
+     */
+    public abstract Matrix invert();
+
+    /**
+     * Called on Matrix A. Computes inverse of A: B = A<sup>-1</sup>
+     * @param B to store the result in
+     * @return B after computing B = A<sup>-1</sup>
+     * @throws IllegalStateException If A is singular an its inverse can not be computed
+     */
+    public abstract Matrix invert(final Matrix B);
+
+
+    /**
+     * Called on Matrix A. Computes A = f(A) element-wise.
+     * @param f Unary higher order function f: x -> y
+     * @return A after computing  A = f(A) element-wise.
+     */
+    public abstract Matrix applyOnElements(final DoubleFunction f);
+
+    /**
+     * Called on Matrix A. Computes B = f(A) element-wise.
+     * @param f Unary higher order function f: x -> y
+     * @param B to store the result in
+     * @return A after computing  B = f(A) element-wise.
+     */
+    public abstract Matrix applyOnElements(final DoubleFunction f, final Matrix B);
+
+    /**
+     * Called on Matrix A. Computes A = f(A, B) element-wise.
+     * @param f Binary higher order function f: x, y -> z
+     * @param B Matrix containing the elements that are used as second arguments in f
+     * @return A after computing  A = f(A, B) element-wise.
+     */
+    public abstract Matrix applyOnElements(final DoubleDoubleFunction f, final Matrix B);
+
+    /**
+     * Called on Matrix A. Computes C = f(A, B) element-wise.
+     * @param f Binary higher order function f: x, y -> z
+     * @param B Matrix containing the elements that are used as second arguments in f
+     * @param C to store the result in
+     * @return A after computing  C = f(A, B) element-wise.
+     */
+    public abstract Matrix applyOnElements(final DoubleDoubleFunction f, final Matrix B, final Matrix C);
+
+    /**
+     * Called on Matrix A. Computes a += v for each row-vector in A.
+     * @param v Vector to add on each row-vector in A
+     * @return A after computing a += v for each row-vector in A.
+     */
+    public abstract Matrix addVectorToRows(final Vector v);
+
+    /**
+     * Called on Matrix A. Computes a += v for each row-vector in A.
+     * @param v Vector to add on each row-vector in A
+     * @param B to store the result in
+     * @return A after computing a += v for each row-vector in A.
+     */
+    public abstract Matrix addVectorToRows(final Vector v, final Matrix B);
+
+    /**
+     * Called on Matrix A. Computes a += v for each col-vector in A.
+     * @param v Vector to add on each col-vector in A
+     * @return A after computing a += v for each col-vector in A.
+     */
+    public abstract Matrix addVectorToCols(final Vector v);
+
+    /**
+     * Called on Matrix A. Computes a += v for each col-vector in A.
+     * @param v Vector to add on each col-vector in A
+     * @param B to store the result in
+     * @return A after computing a += v for each col-vector in A.
+     */
+    public abstract Matrix addVectorToCols(final Vector v, final Matrix B);
+
+    /**
+     * Called on Matrix A. Sets the diagonal entries of A to zero.
+     * @return A after setting its diagonal entries to zero.
+     */
+    public abstract Matrix setDiagonalToZero();
 
     // ---------------------------------------------------
 
@@ -196,15 +346,5 @@ public interface Matrix extends MObject {
 
 
 
-    public abstract Matrix applyOnElements(final MatrixFunction1Arg mf);
 
-    public abstract Matrix applyOnElements(final Matrix m2, final MatrixFunction1Arg mf);
-
-    public abstract Matrix applyOnElements(final Matrix m2, final MatrixFunction2Arg mf);
-
-    public abstract Matrix addVectorToRows(final Vector v);
-
-    public abstract Matrix addVectorToCols(final Vector v);
-
-    public abstract Matrix zeroDiagonal();
 }
