a5f2f044c533db580642da7bb594065553c0a95d
f-sander <fsander@mailbox.tu-berlin.de>
2015-07-22 16:51:23 +0200

    fixed sparse test case

--- pserver-examples/src/main/java/de/tuberlin/pserver/examples/playground/ThreadedMatrixLoaderTestJob.java
+++ pserver-examples/src/main/java/de/tuberlin/pserver/examples/playground/ThreadedMatrixLoaderTestJob.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists;
 import de.tuberlin.pserver.app.PServerJob;
 import de.tuberlin.pserver.app.filesystem.record.IRecordFactory;
 import de.tuberlin.pserver.app.filesystem.record.RecordFormat;
+import de.tuberlin.pserver.app.partitioning.MatrixByRowPartitioner;
 import de.tuberlin.pserver.client.PServerExecutor;
 import de.tuberlin.pserver.examples.ml.GenerateLocalTestData;
 import de.tuberlin.pserver.math.Matrix;
@@ -32,14 +33,15 @@ public final class ThreadedMatrixLoaderTestJob extends PServerJob {
     }
 
     private boolean isOwnPartition(int row, int col, int numNodes) {
-        double numOfRowsPerInstance = (double) GenerateLocalTestData.ROWS_ROWCOLVAL_DATASET / NUM_NODES;
+        double numOfRowsPerInstance = (double) GenerateLocalTestData.ROWS_ROWCOLVAL_DATASET / numNodes;
         double partition = row / numOfRowsPerInstance;
-        return Utils.toInt((long) (partition % NUM_NODES)) == ctx.instanceID;
+        return Utils.toInt((long) (partition % numNodes)) == ctx.instanceID;
     }
 
     @Override
     public void compute() {
         final Matrix matrix = dataManager.getObject("datasets/rowcolval_dataset.csv");
+        Matrix.PartitionShape shape = new MatrixByRowPartitioner(ctx.instanceID, NUM_NODES, GenerateLocalTestData.ROWS_ROWCOLVAL_DATASET, GenerateLocalTestData.COLS_ROWCOLVAL_DATASET).getPartitionShape();
         BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader("datasets/rowcolval_dataset.csv"));
@@ -50,10 +52,10 @@ public final class ThreadedMatrixLoaderTestJob extends PServerJob {
                 int col = Integer.parseInt(parts[1]);
                 double val = Double.parseDouble(parts[2]);
                 if(isOwnPartition(row, col, NUM_NODES)) {
-                    double matrixVal = matrix.get(row, col);
+                    double matrixVal = matrix.get(row % shape.getRows(), col % shape.getCols());
                     if(matrixVal != val) {
                         //throw new RuntimeException(ctx.instanceID + ": matrix("+row+","+col+") is "+matrixVal+" but should be "+val);
-                        System.out.println(ctx.instanceID + ": matrix("+row+","+col+") is "+matrixVal+" but should be "+val);
+                        System.out.println(ctx.instanceID + ": matrix("+row+" % "+shape.getRows()+","+col+" % "+shape.getCols()+") is "+matrixVal+" but should be "+val);
                     }
                     assert(matrixVal == val);
                 }
