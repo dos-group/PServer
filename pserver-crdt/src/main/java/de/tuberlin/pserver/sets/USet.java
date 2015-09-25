dbbe52c353c16dc3018aa6b3302fb527d5028345
patrick.brose <patrick.brose@mailbox.tu-berlin.de>
2015-09-25 13:38:54 -0700

    First working version of USet

---
+++ pserver-crdt/src/main/java/de/tuberlin/pserver/sets/USet.java
@@ -0,0 +1,31 @@
+package de.tuberlin.pserver.sets;
+
+import de.tuberlin.pserver.crdt.Operation;
+import de.tuberlin.pserver.runtime.DataManager;
+
+/**
+ * The Unique Set assumes each value inserted into the set is unique. Hence, there is no need for a tombstone set.
+ */
+
+public class USet<T> extends AbstractUniqueSet {
+
+    public USet(String id, DataManager dataManager) {
+        super(id, dataManager);
+        run(dataManager);
+    }
+
+    @Override
+    protected boolean update(int srcNodeId, Operation op, DataManager dm) {
+        SetOperation<T> sop = (SetOperation<T>)op;
+
+        if(sop.getType() == SetOperation.ADD) {
+            return add(sop.getValue());
+        }
+        else if(sop.getType() == SetOperation.REMOVE) {
+            return remove(sop.getValue());
+        }
+        else {
+            return false;
+        }
+    }
+}
---
+++ pserver-crdt/src/main/java/de/tuberlin/pserver/sets/exceptions/NotUniqueException.java
@@ -0,0 +1,7 @@
+package de.tuberlin.pserver.sets.exceptions;
+
+public class NotUniqueException extends RuntimeException {
+    public NotUniqueException(String message) {
+        super(message);
+    }
+}
