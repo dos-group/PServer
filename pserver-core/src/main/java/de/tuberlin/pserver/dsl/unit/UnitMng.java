aa674b418780a37ef999dbae674c827e336a7a4e
Christoph Alt <christoph.alt@posteo.de>
2015-10-07 15:27:42 +0200

    Loop with termination and sync parameter

--- pserver-core/src/main/java/de/tuberlin/pserver/dsl/unit/UnitMng.java
+++ pserver-core/src/main/java/de/tuberlin/pserver/dsl/unit/UnitMng.java
@@ -46,4 +46,9 @@ public final class UnitMng {
     public static Loop loop(final LoopTermination t, final LoopBody body) throws Exception {
         return new Loop(programContext).exe(t, body);
     }
+
+    public static Loop loop(final LoopTermination t, final int mode, final LoopBody body)
+            throws Exception {
+        return new Loop(programContext).sync(mode).exe(t, body);
+    }
 }
