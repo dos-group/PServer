1bfcdd8115aac308bd52ac9356dcd1596d8dd0f0
patrick.brose <patrick.brose@mailbox.tu-berlin.de>
2016-02-11 13:44:55 +0100

    Deleted matrix and regression packages to fix problems with merge of the new matrix model from master

--- pserver-crdt/src/main/java/de/tuberlin/pserver/matrix/crdt/AvgMatrix64F.java
+++
@@ -1,9 +0,0 @@
-package de.tuberlin.pserver.matrix.crdt;
-
-import de.tuberlin.pserver.math.matrix.Matrix;
-import de.tuberlin.pserver.math.matrix.Matrix64F;
-
-public interface AvgMatrix64F extends Matrix64F {
-
-    void includeInAvg(long row, long col, Double value);
-}
